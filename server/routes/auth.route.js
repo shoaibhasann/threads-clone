8b9dbb97cd74b57fcef5dc9d4ac4c8f445cda93f
shoaibhasann <shoaibhasan0940@gmail.com>
2023-10-23 13:53:02 +0530

    Added full authentication functionality

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import upload from "../middlewares/multer.middleware.js";
-import { changePassword, login, logout, register } from "../controllers/auth.controller.js";
+import { changePassword, forgotPassword, login, logout, register } from "../controllers/auth.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
 router.get("/logout", isLoggedIn, logout);
 router.put("/change-password", isLoggedIn, changePassword);
 
