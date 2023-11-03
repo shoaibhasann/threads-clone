ea6a6d51ca52e0e4eb605092121bc7478d9c1048
shoaibhasann <shoaibhasan0940@gmail.com>
2023-11-03 19:17:17 +0530

    Setup redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import App from "./App.jsx";
+import store from "./store/Store.js";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <ToastContainer position="bottom-left" autoClose={2000} theme="colored" />
+    </BrowserRouter>
+  </Provider>
+);
---
+++ client/src/store/Store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: {
+
+    },
+    devTools: true,
+});
+
+export default store;
