6b10e5f87694d8074924df4dba0992b088ef10d8
Mohd Shoaib <112479144+shoaibhasann@users.noreply.github.com>
2026-01-20 15:34:15 +0530

    Enhance Axios instance with request interceptor
    Added request interceptor to include authorization token.

--- client/src/helpers/AxiosInstance.js
+++ client/src/helpers/AxiosInstance.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_SERVER_URL
+const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,
-    headers: {
-        common: {
-            "Content-Type": "application/json", // Set the defaut content type to JSON
-        }
-    }
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosInstance;
